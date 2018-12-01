0bf239be6494b8adf64eb0c21b9cfbfe72ae9053
Rachelle <43661757+mrachelle@users.noreply.github.com>
2018-11-30 20:04:16 -0700

    exercise using boolean logic and operators
    simple exercise using boolean logic and operators to see whether or not an applicant would qualify for insurance based on set parameters

---
+++ booleanOperators.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string personsAge = Console.ReadLine();
+            int age = Convert.ToInt32(personsAge);
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            string personsTickets = Console.ReadLine();
+            int tickets = Convert.ToInt32(personsTickets);
+
+            Console.WriteLine("Qualifies?");
+            bool qualifies = (age > 15 && tickets == 0 && dui == "false");
+            Console.WriteLine(qualifies);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
