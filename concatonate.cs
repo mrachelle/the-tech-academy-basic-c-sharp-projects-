e2d93d85bd3419712e5da301b0616381ea94a976
Rachelle <43661757+mrachelle@users.noreply.github.com>
2018-12-11 21:19:43 -0700

    small exercise in concatenation
    concatenate a few strings

---
+++ concatonate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Bender Bending Rodriguez";
+            int age = 4;
+            string composition = "zinc, titanium, iron, dolomite, lead, empty, back ";
+            name = name.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Robot's Name: " + name + ". Robot's Age: " + age + ". Robot's composition: " + composition);
+            
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
