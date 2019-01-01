163dde375b1f07876fb777da558507dfcc3b3ac6
Rachelle <43661757+mrachelle@users.noreply.github.com>
2019-01-01 12:54:08 -0700

    logging to a text file
    logs input into a text file

---
+++ LogToTextFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please pick a number");
+            string text = Console.ReadLine();
+            System.IO.File.WriteAllText(@"C:\Users\mariv\Desktop\NOTES\log.txt", text);
+
+        }
+    }
+}
