99b6c6e3a4d96930693649c038467b7f8cc05432
Rachelle <43661757+mrachelle@users.noreply.github.com>
2018-12-31 18:05:48 -0700

    what day of the week is it
    asks what day of the week it is and prompts the user to re input the info if it does not match the enum for days of the week

---
+++ whatDay.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What day of the week is it?");
+        string input = Console.ReadLine();
+        DayOfTheWeek day;
+            
+        try
+        {
+            day = (DayOfTheWeek)Enum.Parse(typeof(DayOfTheWeek), input);
+        }
+        catch
+        {
+            Console.WriteLine("Please enter an actual day of the week");
+        }
+
+        Console.ReadLine(); 
+    }
+    public enum DayOfTheWeek
+    {
+        Sunday=0,
+        Monday=1,
+        Tuesday=2,
+        Wednesday=3,
+        Thursday=4,
+        Friday=5,
+        Saturday=0
+    }
+}
+
