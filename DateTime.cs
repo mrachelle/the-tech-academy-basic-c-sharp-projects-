3fb40730ea7742482e8e38c676350f1156767e9d
Rachelle <43661757+mrachelle@users.noreply.github.com>
2019-01-01 13:54:30 -0700

    datetime
    prints current datetime and addhours

---
+++ DateTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Pick a whole number");
+            double value = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In " + value + " hours the time will be " + DateTime.Now.AddHours(0 + value));
+            Console.ReadKey();
+        }
+    }
+}
