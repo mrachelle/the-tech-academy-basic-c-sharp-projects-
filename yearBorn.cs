f3345123154c37484777980b3f125da4af6598df
Rachelle <43661757+mrachelle@users.noreply.github.com>
2019-01-01 15:32:52 -0700

    year you were born
    asks for the users age. displays the year the user was born

---
+++ yearBorn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YearBorn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                if (age <= 0) Console.WriteLine("Please pick a positive number greater than 0");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            int difference = (2019 - age);
+            Console.WriteLine("You were born in the year " + difference);
+            Console.ReadLine();
+
+        }
+    }
+}
